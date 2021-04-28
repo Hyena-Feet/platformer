1c221797ef0d1aaf7f2ad1d616cbc489eba32d80
Hyena-Feet <82405199+Hyena-Feet@users.noreply.github.com>
2021-04-28 13:58:16 -0400

    Cooler bigger tilemap

--- main.ts
+++ main.ts
@@ -1,11 +1,30 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (jumps < 2) {
+    if (jumps < 1) {
         mySprite.vy += -300
         jumps += 1
     }
 })
-scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, location) {
+    if (controller.up.isPressed()) {
+        mySprite.vy = -50
+    } else {
+        mySprite.vy = 50
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, location) {
+    if (controller.A.isPressed()) {
+        tiles.setTileAt(location, assets.tile`myTile10`)
+        game.splash("There's a Note Inside")
+        game.showLongText("Be Wary, There's something in this cave with us. If you find this I don't care who you are or why you're in this God-Forsaken but I'm taking my men out of here, no amount of money will bring us back; I advise you do the same.", DialogLayout.Full)
+    }
+})
+function Level1 () {
 	
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile11`, function (sprite, location) {
+    if (controller.A.isPressed()) {
+        game.splash("These Vines are Climbable")
+    }
 })
 let jumps = 0
 let mySprite: Sprite = null
@@ -36,7 +55,7 @@ controller.moveSprite(mySprite, 100, 0)
 scene.cameraFollowSprite(mySprite)
 mySprite.ay = 1000
 jumps = 0
-tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 12))
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 16.5))
 forever(function () {
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
         jumps = 0
